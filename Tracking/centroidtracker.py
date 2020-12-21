0f77311dc0c8f6d45cc160cc887c830767f1af00
zZChopperZz <vanspykid85@gmail.com>
2020-12-21 12:58:20 +0700

    Update centroidtracker.py

--- Tracking/centroidtracker.py
+++ Tracking/centroidtracker.py
@@ -1,153 +1,110 @@
-# import the necessary packages
-from scipy.spatial import distance as dist
-from collections import OrderedDict
+from scipy.spatial import distance as dist		#Dùng để tính khoảng cách
+from collections import OrderedDict				#Sử dụng giống như mảng, nhưng ghi nhớ thứ tự các đối tượng truyền vào
 import numpy as np
 
 class CentroidTracker():
 	def __init__(self, maxDisappeared=50):
-		# initialize the next unique object ID along with two ordered
-		# dictionaries used to keep track of mapping a given object
-		# ID to its centroid and number of consecutive frames it has
-		# been marked as "disappeared", respectively
-		self.nextObjectID = 0
-		self.objects = OrderedDict()
-		self.disappeared = OrderedDict()
+		# maxDisappeared: số lượng khung hình liên tiếp tối đa mà một đối tượng phải mất đi khi không được track nữa
+		self.nextObjectID = 0		#ID mới, chưa được dùng để đăng ký
+		self.objects = OrderedDict()	#chưa ID và trọng tâm của đối tượng đó
+		self.disappeared = OrderedDict()	#số lượng khung hình liên tiếp của 1 đối tượng bị đánh dấu là mất
 
-		# store the number of maximum consecutive frames a given
-		# object is allowed to be marked as "disappeared" until we
-		# need to deregister the object from tracking
 		self.maxDisappeared = maxDisappeared
 
 	def register(self, centroid):
-		# when registering an object we use the next available object
-		# ID to store the centroid
+		# đăng ký theo dõi đối tượng với ID mới
 		self.objects[self.nextObjectID] = centroid
 		self.disappeared[self.nextObjectID] = 0
-		self.nextObjectID += 1
+		self.nextObjectID += 1 # tăng giá trị để gán cho ID tiếp theo
 
 	def deregister(self, objectID):
-		# to deregister an object ID we delete the object ID from
-		# both of our respective dictionaries
 		del self.objects[objectID]
 		del self.disappeared[objectID]
 
-	def update(self, rects):
-		# check to see if the list of input bounding box rectangles
-		# is empty
+	def update(self, rects): #Tham so truyen vao la cac bounding box
+		#Neu khong co bounding box nao trong list
 		if len(rects) == 0:
-			# loop over any existing tracked objects and mark them
-			# as disappeared
+			# duyệt qua danh sách các đối tượng đã theo dõi và tăng giá trị disappeared lên
 			for objectID in list(self.disappeared.keys()):
 				self.disappeared[objectID] += 1
 
-				# if we have reached a maximum number of consecutive
-				# frames where a given object has been marked as
-				# missing, deregister it
+				# khi giá trị disappeared dat den maxDisappeared thì hủy đăng ký
 				if self.disappeared[objectID] > self.maxDisappeared:
 					self.deregister(objectID)
 
-			# return early as there are no centroids or tracking info
-			# to update
 			return self.objects
+ 
+		inputCentroids = np.zeros((len(rects), 2), dtype="int")	#khởi tạo mảng 0 để lưu trữ các giá trị trọng tâm cho
+		# từng bounding box
 
-		# initialize an array of input centroids for the current frame
-		inputCentroids = np.zeros((len(rects), 2), dtype="int")
-
-		# loop over the bounding box rectangles
+		# duyệt qua từng bounding box trong rects
 		for (i, (startX, startY, endX, endY)) in enumerate(rects):
-			# use the bounding box coordinates to derive the centroid
+			# lấy ra tọa độ trọng tâm của box và lưu vào inputCentroids
 			cX = int((startX + endX) / 2.0)
 			cY = int((startY + endY) / 2.0)
 			inputCentroids[i] = (cX, cY)
 
-		# if we are currently not tracking any objects take the input
-		# centroids and register each of them
+		# nếu chưa theo dõi bất kì đối tượng nào thì tiến hành đăng ký đối tượng mới
 		if len(self.objects) == 0:
 			for i in range(0, len(inputCentroids)):
 				self.register(inputCentroids[i])
 
-		# otherwise, are are currently tracking objects so we need to
-		# try to match the input centroids to existing object
-		# centroids
+		# ngược lại, cập nhật lại các đối tượng đã được đăng ký rồi
 		else:
-			# grab the set of object IDs and corresponding centroids
+			# lấy các giá trị ID và giá trị trọng tâm
 			objectIDs = list(self.objects.keys())
 			objectCentroids = list(self.objects.values())
 
-			# compute the distance between each pair of object
-			# centroids and input centroids, respectively -- our
-			# goal will be to match an input centroid to an existing
-			# object centroid
+			# tính khoảng cách của các trọng tâm của các đối tượng đã theo dõi và các đối tượng chưa theo dõi
 			D = dist.cdist(np.array(objectCentroids), inputCentroids)
 
-			# in order to perform this matching we must (1) find the
-			# smallest value in each row and then (2) sort the row
-			# indexes based on their minimum values so that the row
-			# with the smallest value as at the *front* of the index
-			# list
+			# tìm giá trị min của mỗi hàng và sắp xếp cho index của giá trị min lên đầu hàng
 			rows = D.min(axis=1).argsort()
 
-			# next, we perform a similar process on the columns by
-			# finding the smallest value in each column and then
-			# sorting using the previously computed row index list
+			# thực hiện tương tự đối với các cột, sắp xếp dựa trên hàng
 			cols = D.argmin(axis=1)[rows]
 
-			# in order to determine if we need to update, register,
-			# or deregister an object we need to keep track of which
-			# of the rows and column indexes we have already examined
+			# tạo 2 set chứa các index đã sử dụng
 			usedRows = set()
 			usedCols = set()
 
-			# loop over the combination of the (row, column) index
-			# tuples
+			# duyệt qua các tập index hàng và cột
 			for (row, col) in zip(rows, cols):
-				# if we have already examined either the row or
-				# column value before, ignore it
-				# val
+				# chỉ mục nào đã được sử dụng thì bỏ qua
 				if row in usedRows or col in usedCols:
 					continue
 
-				# otherwise, grab the object ID for the current row,
-				# set its new centroid, and reset the disappeared
-				# counter
+				# nếu chưa dùng thì lấy giá trị ID của index hiện tại,set làm trọng tâm mới: row là ID, cột là trọng tâm
 				objectID = objectIDs[row]
 				self.objects[objectID] = inputCentroids[col]
 				self.disappeared[objectID] = 0
 
-				# indicate that we have examined each of the row and
-				# column indexes, respectively
+				# cập nhật lại các chỉ mục hàng và cột đã sử dụng
 				usedRows.add(row)
 				usedCols.add(col)
 
-			# compute both the row and column index we have NOT yet
-			# examined
+			# xử lý các centroid indexs chưa được dùng
 			unusedRows = set(range(0, D.shape[0])).difference(usedRows)
 			unusedCols = set(range(0, D.shape[1])).difference(usedCols)
 
-			# in the event that the number of object centroids is
-			# equal or greater than the number of input centroids
-			# we need to check and see if some of these objects have
-			# potentially disappeared
+			# Nếu số lượng các trọng tâm của đối tượng trong objectCentroids lớn hơn hoặc bằng số lượng các trọng tâm trong inputCentroids 
+			# inputCentroids chứa các trọng tâm của các bounding box trong frame, còn objectCentroids chứa các trọng tâm đã đăng ký rồi
+			# => có các đối tượng đã bị mất track.
 			if D.shape[0] >= D.shape[1]:
-				# loop over the unused row indexes
+				# duyệt qua các index chưa được dùng, lấy giá trị ID và tăng giá trị disappeared lên
 				for row in unusedRows:
-					# grab the object ID for the corresponding row
-					# index and increment the disappeared counter
 					objectID = objectIDs[row]
 					self.disappeared[objectID] += 1
 
-					# check to see if the number of consecutive
-					# frames the object has been marked "disappeared"
-					# for warrants deregistering the object
+					# Hủy đăng ký đối tượng trong khi disappeared đạt tối đa
 					if self.disappeared[objectID] > self.maxDisappeared:
 						self.deregister(objectID)
 
-			# otherwise, if the number of input centroids is greater
-			# than the number of existing object centroids we need to
-			# register each new input centroid as a trackable object
+			# ngược lại, D.shape(objectCentroids) < D.shape(inputCentroids), số lượng inputCentroids lớn hơn số lượng objectCentroids đã tồn tại
+			# => có thêm đối tượng mới, đăng ký theo dõi các đối tượng mới
 			else:
 				for col in unusedCols:
 					self.register(inputCentroids[col])
 
-		# return the set of trackable objects
+		# trả về các đối tượng đã được theo dõi
 		return self.objects
